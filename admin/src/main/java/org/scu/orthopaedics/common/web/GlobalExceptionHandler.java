5ce9c5044ef2208b6188db088b8f665a2b1ec323
Leon-Chen <leon_chen2003@163.com>
2024-02-28 16:04:44 +0800

    feature: 开发全局异常处理器

---
+++ admin/src/main/java/org/scu/orthopaedics/common/web/GlobalExceptionHandler.java
@@ -0,0 +1,82 @@
+package org.scu.orthopaedics.common.web;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.scu.orthopaedics.common.errorcode.BaseErrorCode;
+import org.scu.orthopaedics.common.exception.AbstractException;
+import org.scu.orthopaedics.common.result.Result;
+import org.scu.orthopaedics.common.result.Results;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * 全局异常处理器
+ */
+@Component("globalExceptionHandlerByAdmin")
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 拦截参数验证异常
+     */
+    @SneakyThrows
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public Result validExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        FieldError firstFieldError = CollectionUtil.getFirst(bindingResult.getFieldErrors());
+        String exceptionStr = Optional.ofNullable(firstFieldError)
+                .map(FieldError::getDefaultMessage)
+                .orElse(StrUtil.EMPTY);
+        log.error("[{}] {} [ex] {}", request.getMethod(), getUrl(request), exceptionStr);
+        return Results.failure(BaseErrorCode.CLIENT_ERROR.code(), exceptionStr);
+    }
+
+    /**
+     * 拦截应用内抛出的异常
+     */
+    @ExceptionHandler(value = {AbstractException.class})
+    public Result abstractException(HttpServletRequest request, AbstractException ex) {
+        if (ex.getCause() != null) {
+            log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString(), ex.getCause());
+            return Results.failure(ex);
+        }
+        log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString());
+        return Results.failure(ex);
+    }
+
+    /**
+     * 拦截未捕获异常
+     */
+    @ExceptionHandler(value = Throwable.class)
+    public Result defaultErrorHandler(HttpServletRequest request, Throwable throwable) {
+        log.error("[{}] {} ", request.getMethod(), getUrl(request), throwable);
+        // 注意，此处是为了聚合模式添加的代码，正常不需要该判断
+        if (Objects.equals(throwable.getClass().getSuperclass().getSimpleName(), AbstractException.class.getSimpleName())) {
+            String errorCode = ReflectUtil.getFieldValue(throwable, "errorCode").toString();
+            String errorMessage = ReflectUtil.getFieldValue(throwable, "errorMessage").toString();
+            return Results.failure(errorCode, errorMessage);
+        }
+        return Results.failure();
+    }
+
+    private String getUrl(HttpServletRequest request) {
+        if (StringUtils.isEmpty(request.getQueryString())) {
+            return request.getRequestURL().toString();
+        }
+        return request.getRequestURL().toString() + "?" + request.getQueryString();
+    }
+}
+
